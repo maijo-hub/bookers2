8558844483d55560af32c6254c0ede6ac2fd4d68
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-06-04 07:43:26 +0000

    [Add]bookers2top画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else 
+      render :edit
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :books, only: [:new, :create, :update, :index, :show, :destroy, :edit]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
