3f962dee1cd58f7e70530e72d95c6cec02ebccec
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-06-05 07:40:24 +0000

    [Finish]bookers2 usersのedit

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
 
   resources :books, only: [:new, :create, :update, :index, :show, :destroy, :edit]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
