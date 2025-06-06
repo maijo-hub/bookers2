aeff35f4763865e39086da2bc2dff0e1c4b1c5ef
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-06-06 00:59:58 +0000

    [Add]bookers2バリデーション新規投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      render:new
+    end
   end
 
   def index
@@ -25,6 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book)
     else 
       render :edit
